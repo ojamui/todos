c80d2fc73a36f799140230a947e393ebc763dd77
ojamui <ophirjamui@gmail.com>
2018-11-05 20:49:42 +0200

    WIP: backend logic to get/add/update/remove item from db

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -8,31 +8,56 @@ use App\Http\Requests;
 
 class TodoController extends Controller
 {
-    //Methods shouldd be routed to here from routes.php
 
-    //Get all todos (GET)
+    public function getTodos()
+    {
+        $todos = Todo::all();
+        return $todos->toJson(JSON_PRETTY_PRINT);
+    }
 
-    //Add new Todo (PUT)             ***  MAYBE DECIDE HERE?
     public function putTodos(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required|max:255'
+        ]);
+
         $title = $request->input('title');
 
-        if($request->has('id')){ // TODO: AND ID EXISTS
-            return 'Should UPDATE';
+        if($request->has('id') ){
+
+            $todo = Todo::find($request->input('id'));
+            if($todo){
+                if($request->has('isDone') && $todo->isDone !== $request->input('isDone')){
+                    $todo->isDone = $request->input('isDone');
+                }
+                if($todo->title !== $request->input('title')){
+                    $todo->title = $request->input('title');
+                }
+            }
+
+        } else {
+            $todo = new Todo();
+            $todo->title = $title;
+            $todo->isDone = false;
         }
         
-        //NEW TODO
-        $todo = new Todo();
-
-        $todo->title = $title;
-        $todo->isDone = false;
-
         $todo->save();
 
-        return 'New TODO saved; Should return new list';
+        return $this->getTodos();
 
     }
-    //Update existing Todo (PUT)     ***
     
-    //Delete existing Todo (DELETE)
+    public function deleteTodos($id){
+        
+        // delete all
+        // Todo::truncate();
+
+        $todo = Todo::find($id);
+        if($todo){
+            $todo->delete();
+        }
+
+        return $this->getTodos();
+    }
+
 }
