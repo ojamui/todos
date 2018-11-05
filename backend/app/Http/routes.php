28ce56575caf598360fbca4b25c4c92dbabd6aed
ojamui <ophirjamui@gmail.com>
2018-11-05 20:47:33 +0200

    finished refactoring logic to controller

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -13,42 +13,12 @@
 
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Get All todos
-Route::get('/api/todos', function()
-{
-    $mockList = [ // GET FROM DB
-        [
-            'id' => 123,
-            'title' => 'First Task',
-            'isDone' => true      
-        ], [
-            'id' => 124,
-            'title' => '2nd Task',
-            'isDone' => false 
-        ]
-    ];
-    return response()->json($mockList);
-});
-
-// Add/Update Single todo
 Route::put('/api/todos', [
     'uses' => 'TodoController@putTodos'
 ]);
-// Route::put('/api/todos', function (Request $request)
-// {
-//     //     return response()->json(['title' => $request->title]);
-//     if($request->has('id')){  // MAYBE SEND DECISION TO CONTROLLER
-//         return 'Should Update';
-//     }
-//     return 'Should Create';
-// });
-
-// Delete Single todo
-// Route::delete('/api/todos/{todo}', function($id) 
-// {
-//     return $id;
-// });
+Route::get('/api/todos', [
+    'uses' => 'TodoController@getTodos'
+]);
+Route::delete('/api/todos/{todo}',[
+    'uses' => 'TodoController@deleteTodos'
+]);
